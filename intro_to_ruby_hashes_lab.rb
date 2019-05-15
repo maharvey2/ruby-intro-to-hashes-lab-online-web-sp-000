a2a52b9bc33ff4c798a871886db76d17edd52d2e
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-15 14:40:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,17 +6,8 @@ def actor
  actor = {name: "Dwayne The Rock Johnson"}
 end
 
-def monopoly {
-  :railroads=>
-    {
-      :pieces=>4,
-      :rent_in_dollars=>
-      {
-        :one_piece_owned=>25,
-        :two_pieces_owned=>50,
-        :three_pieces_owned=>100,
-        :four_pieces_owned=>200
-      },
+def monopoly {}
+
 end
 
 def monopoly_with_second_tier
